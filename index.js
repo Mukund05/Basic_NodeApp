ad6116e78cbbbab6b9ef44c52eb4a9b4025001e4
MUKUND THAKUR <92576255+Mukund05@users.noreply.github.com>
2023-10-03 17:22:36 +0530

    Pushed Forget and reset api logic

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-// index.js
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const dotenv = require('dotenv');
-const bcrypt = require('bcrypt');
-dotenv.config();
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const randomstring = require("randomstring");
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Connect to MongoDB (replace 'your-database-url' with your actual database URL)
-mongoose.connect("your-database-url", { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(bodyParser.json());
+// Connect to MongoDB (replace 'MONGODB_URI' with your actual database URL in env file)
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// Middleware for JSON parsing
+
 app.use(express.json());
 
-// Passport initialization
-app.use(passport.initialize());
-
-// User model (replace with your actual User model)
-const User = mongoose.model('User', new mongoose.Schema({
+// User model 
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
     username: {
       type: String,
       required: true,
@@ -33,146 +37,222 @@ const User = mongoose.model('User', new mongoose.Schema({
       type: String,
       required: true,
     },
-    role: String,
-}));
-
-// Set up Passport Local Strategy for username/password login
-passport.use(new LocalStrategy({
-    usernameField: 'username',
-    passwordField: 'password',
-  }, async (username, password, done) => {
-    try {
-      const user = await User.findOne({ username });
-      if (!user) {
-        return done(null, false, { message: 'Invalid username or password' });
-      }
-  
-      if (!user.authenticate(password)) {
-        return done(null, false, { message: 'Invalid username or password' });
-      }
-  
-      return done(null, user);
-    } catch (error) {
-      return done(error);
-    }
-  }));
-
-
+    token: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  })
+);
 
 // Create a new user
-app.post('/signup', async (req, res) => {
-    try {
-        const { username, password, email } = req.body;
+app.post("/signup", async (req, res) => {
+  try {
+    const { username, password, email } = req.body;
+
+    if (!username || !password || !email) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+
+    // password hashing
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //new user with the hashed password
+    const newUser = new User({ username, password: hashedPassword, email });
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.json({ message: "Registration successful", user: newUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Login route
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
     
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
-    
-        // Create a new user with the hashed password
-        const newUser = new User({ username, password: hashedPassword, email });
-        
-        // Save the user to the database
-        await newUser.save();
-    
-        res.json({ message: 'Registration successful', user: newUser });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
-      }
-  });
-  
-  //login user
-  // Login route
-app.post('/login', async (req, res) => {
-    try {
-      const { username, password } = req.body;
-  
-      // Find the user by username
-      const user = await User.findOne({ username });
-  
-      // Check if the user exists
-      if (!user) {
-        return res.status(401).json({ message: 'Invalid credentials' });
-      }
-  
-      // Compare the provided password with the stored hashed password
-      const passwordMatch = await bcrypt.compare(password, user.password);
-  
-      if (passwordMatch) {
-        // Passwords match, generate a token and send it in the response
-        const token = jwt.sign({ id: user._id, username: user.username }, 'mkd#123', { expiresIn: '1h' });
-        res.json({ token });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordMatch) {
+      res.json({ message: "Login successful" });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Get all users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Get user by ID
+app.get("/users/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Update a user by ID
+app.put("/users/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const { password } = req.body;
+    // Check if the update includes the password field then return an error
+    if (password) {
+      return res
+        .status(400)
+        .json({ message: "Password cannot be updated using this route" });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+      new: true,
+    });
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json({ message: "User updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Delete a user by ID
+app.delete("/users/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Nodemailer config
+const sendMail = async (email, resetToken) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: true,
+      requireTLS: true,
+      auth: {
+        user: process.env.TEST_EMAIL,
+        pass: process.env.TEST_PASSWORD,
+      },
+    });
+
+    const emailBody = `
+    Click on the following link to reset your password:
+    http://localhost:3000/reset-password/${resetToken}`;
+
+    // Compose the reset password email
+    const mailoption = {
+      from: process.env.TEST_EMAIL,
+      to: email,
+      subject: "Password reset",
+      text: emailBody,
+    };
+
+    transporter.sendMail(mailoption, function (error, info) {
+      if (error) {
+        console.log(error);
       } else {
-        // Passwords do not match
-        res.status(401).json({ message: 'Invalid credentials' });
+        console.log("Email sent successfully: " + info.response);
       }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
+    });
+  } catch (error) {
+    res.status(400).json({ message: "Internal Server Error" });
+  }
+};
 
+// Forget Password route
+app.post("/forget-password", async (req, res) => {
+  const { email } = req.body;
 
-  // Get all users
-  app.get('/users', async (req, res) => {
-    try {
-      const users = await User.find();
-      res.json(users);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
-  
-  // Get a user by ID
-  app.get('/users/:id', async (req, res) => {
-    const userId = req.params.id;
-  
-    try {
-      const user = await User.findById(userId);
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.json(user);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
-  
-  // Update a user by ID
-  app.put('/users/:id', async (req, res) => {
-    const userId = req.params.id;
-  
-    try {
-      const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true });
-      if (!updatedUser) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.json(updatedUser);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
-  
-  // Delete a user by ID
-  app.delete('/users/:id', async (req, res) => {
-    const userId = req.params.id;
-  
-    try {
-      const deletedUser = await User.findByIdAndDelete(userId);
-      if (!deletedUser) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.json({ message: 'User deleted successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
+  try {
+    const user = await User.findOne({ email });
 
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
 
+    // Generating a unique reset token for the user
+    const resetToken = randomstring.generate();
 
+    user.token = resetToken;
+    await user.save();
+    sendMail(email, resetToken);
+
+    res.json({ message: "Reset email sent successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Reset Password route
+app.post("/reset-password/:token", async (req, res) => {
+  const { token } = req.params;
+  const { newPassword } = req.body;
+
+  try {
+    // Find the user with the help of token
+    const user = await User.findOne({ token: token });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "Invalid or expired reset token" });
+    }
+
+    // Update the user's password and hash it  and remove the reset token to null or empty
+    user.password = await bcrypt.hash(newPassword, 10);
+    user.token = "";
+    await user.save();
+
+    res.json({ message: "Password reset successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 // Start the server
 app.listen(PORT, () => {
